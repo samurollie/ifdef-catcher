2e75dc785a1bfc8e93f07b78685d418099258fc2
jcarlosadm <jcvf@ic.ufal.br>
2021-02-08 23:11:33 -0300

    update Main

--- phase-1-explore/detector/src/main/java/detector/Main.java
+++ phase-1-explore/detector/src/main/java/detector/Main.java
@@ -2,8 +2,8 @@ package detector;
 
 public class Main {
     public static void main(String[] args) {
-        Detector detector = new Detector();
-        for (PrepRefactored prepRefactored : detector.getPrepRefactoredList()) {
+        var detector = new Detector();
+        for (var prepRefactored : detector.getPrepRefactoredList()) {
             // TODO print refactoring
         }
     }
